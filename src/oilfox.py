f7ff09d18615e0c306d127f6e9fc50df087efa2b
tringler <thomas.ringler@googlemail.com>
2021-03-28 23:44:20 +0200

    Add exception classes

--- src/oilfox.py
+++ src/oilfox.py
@@ -4,71 +4,85 @@ import json
 import jwt
 import time
 
-class api():
+
+class api:
     def __init__(self, email, password):
         self.email = email
         self.password = password
 
     def login(self):
         url = "https://api.oilfox.io/v3/login"
-        logindata = {
-            "email": self.email,
-            "password": self.password
-        }
+        logindata = {"email": self.email, "password": self.password}
         logindata = json.dumps(logindata)
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json','User-Agent': 'oilfoxpy', 'Connection': 'Keep-Alive'}
+        headers = {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+            "User-Agent": "oilfoxpy",
+            "Connection": "Keep-Alive",
+        }
         response = requests.post(url, data=logindata, headers=headers, timeout=10)
-        if(response.status_code == 200):
+        if response.status_code == 200:
             response = json.loads(response.text)
-            self.access_token = response['access_token']
-            self.refreshtoken = response['refresh_token']
+            self.access_token = response["access_token"]
+            self.refreshtoken = response["refresh_token"]
             return True
-        elif(response.status_code == 404):
-            raise oilfoxerror('Wrong e-mail')
-        elif(response.status_code == 401):
-            raise oilfoxerror('Wrong password')
+        elif response.status_code >= 500:
+            raise ApiUnreachable
+        elif response.status_code == 404:
+            raise InvalidMailAddress
+        elif response.status_code == 401:
+            raise InvalidPassword
         else:
             self.login = False
             return False
 
-
     def getSummary(self):
-        if(hasattr(self, 'access_token')):
+        if hasattr(self, "access_token"):
             self.reqrefreshtoken()
-            url= "https://api.oilfox.io/v4/summary"
-            headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Connection': 'Keep-Alive','User-Agent': 'oilfoxpy', 'Authorization': 'Bearer ' + self.access_token}
+            url = "https://api.oilfox.io/v4/summary"
+            headers = {
+                "Content-type": "application/json",
+                "Accept": "application/json",
+                "Connection": "Keep-Alive",
+                "User-Agent": "oilfoxpy",
+                "Authorization": "Bearer " + self.access_token,
+            }
             response = requests.get(url, headers=headers, timeout=10)
-            if(response.status_code == 200):
+            if response.status_code == 200:
                 response = json.loads(response.text)
                 return response
-            #elif(response.status_code == 401):
+            # elif(response.status_code == 401):
             #    refreshtoken
             else:
                 return False
         else:
-            raise oilfoxerror('Login first')
+            raise InvalidAuthToken
             return False
 
     def reqrefreshtoken(self):
-        if(hasattr(self, 'access_token')):
+        if hasattr(self, "access_token"):
             dectoken = jwt.decode(self.access_token, verify=False)
             ts = time.time()
-            if(dectoken['exp'] <= int(ts)):
+            if dectoken["exp"] <= int(ts):
                 url = "https://api.oilfox.io/v3/token"
-                headers = {'Content-type': 'application/json', 'Accept': 'application/json','User-Agent': 'oilfoxpy', 'Connection': 'Keep-Alive'}
-                token = {
-                    'refresh_token': self.refreshtoken
+                headers = {
+                    "Content-type": "application/json",
+                    "Accept": "application/json",
+                    "User-Agent": "oilfoxpy",
+                    "Connection": "Keep-Alive",
                 }
+                token = {"refresh_token": self.refreshtoken}
                 response = requests.post(url, params=token, headers=headers, timeout=10)
-                if(response.status_code == 200):
+                if response.status_code == 200:
                     response = json.loads(response.text)
-                    self.access_token = response['access_token']
-                    self.refreshtoken = response['refresh_token']
+                    self.access_token = response["access_token"]
+                    self.refreshtoken = response["refresh_token"]
                     return True
                 else:
-                    raise oilfoxerror('failed tokenrefresh')
+                    raise oilfoxerror("failed tokenrefresh")
 
-class oilfoxerror(Exception):
+
+class OilfoxError(Exception):
     def __init__(self, *args):
         if args:
             self.message = args[0]
@@ -76,8 +90,33 @@ class oilfoxerror(Exception):
             self.message = None
 
     def __str__(self):
-        print('calling str')
+        print("calling str")
         if self.message:
-            return 'Oilfox, {0} '.format(self.message)
+            return "Oilfox, {0} ".format(self.message)
         else:
-            return 'Oilfox error'
+            return "Oilfox error"
+
+
+class InvalidPassword(OilfoxError):
+    self.message = "Invalid password"
+    pass
+
+
+class InvalidMailAddress(OilfoxError):
+    self.message = "Invalid e-mail address"
+    pass
+
+
+class ApiUnreachable(OilfoxError):
+    self.message = "API unreachable"
+    pass
+
+
+class AuthTokenRefreshFailed(OilfoxError):
+    self.message = "Token refresh failed"
+    pass
+
+
+class InvalidAuthToken(OilfoxError):
+    self.message = "AuthToken invalid"
+    pass
